fe94931f9af6a03e4e471ede46a7206c834e644c
Cai Davies <caidavies37@gmail.com>
2025-07-30 18:08:26 +0200

    Initial commit

--- app/(protected)/(tabs)/index.tsx
+++ app/(protected)/(tabs)/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 	return (
 		<View className="flex-1 items-center justify-center bg-background p-4 gap-y-4">
 			<H1 className="text-center">Home</H1>
-			<Muted className="text-center">
+			<Muted className="text-center text-2xl">
 				You are now authenticated and this session will persist even after
 				closing the app.
 			</Muted>
